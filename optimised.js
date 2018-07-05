24c13fd8bd476fc8cdc7f1c19767182a8ab0a605
Srivigneshwar <prasadsrivi@gmail.com>
2018-07-05 18:15:47 +0100

    add shadow

--- optimised.js
+++ optimised.js
@@ -2,11 +2,10 @@
 
 var deckoLogo = function() {
   // opts = typeof opts !== 'undefined' ? opts : {};
-  // this.logoCards = new Array(4);
   // var rotationDegrees = ['r355', 'r345', 'r332', 'r318', 'r202']
   this.s = Snap("#decko-animated-logo");
   this.rotationDegrees = ['r345', 'r332', 'r318', 'r302'];
-  this.colors = ['black', '#ebebeb'];
+  this.colors = ['#593196', '#fafafa'];
   this.css_id = "js-svg-card-";
   this.cp = [ // x, y, width, height, roundcornerX, roundcornerY
     // [0, 0, 202, 164, 12.5, 12.5],
@@ -18,6 +17,10 @@ var deckoLogo = function() {
   this.shadow = this.s.filter(Snap.filter.shadow(1, 0, 11, "#00000", 0.1));
   this.duration = 1000;
   this.easing = mina.bounce;
+  this.shadow = this.s.filter(Snap.filter.shadow(1, 0, 11, "#00000", 0.1));
+  this.enableShadow = true;
+  this.stacked = true;
+  this.resizeOnCreate = false;
 }
 
 var _deckoLogo = deckoLogo.prototype;
@@ -28,30 +31,23 @@ _deckoLogo.createlogoCards = function() {
     colors = this.colors,
     css_id = this.css_id,
     rotationDegrees = this.rotationDegrees,
-    s = this.s;
+    s = this.s,
+    stacked = this.stacked,
+    roc = this.resizeOnCreate,
+    es = this.enableShadow,
+    shadow = this.shadow;
 
   for (var i = 0; i < logoCards.length; i++) {
-    logoCards[i] = s.rect(cp[i][0], cp[i][1], cp[i][2], cp[i][3], cp[i][4], cp[i][5]).attr({
-      fill: (i % 2 === 0) ? colors[0] : colors[1],
-      id: css_id + "i"
-      // filter: shadow
-    }).transform(rotationDegrees[i]);
-  }
-}
+    var x = roc ? cp[i][0] : "0",
+        y = roc ? cp[i][1] : "0",
+        width = roc ? cp[i][2] : "202",
+        height = roc ? cp[i][3] : "164";
 
-_deckoLogo.createlogoCards2 = function() {
-  var cp = this.cp,
-    colors = this.colors,
-    css_id = this.css_id,
-    rotationDegrees = this.rotationDegrees,
-    s = this.s;
-
-  for (var i = 0; i < logoCards.length; i++) {
-    logoCards[i] = s.rect(0, 0, 202, 164, cp[i][4], cp[i][5]).attr({
+    logoCards[i] = s.rect(x, y, width, height, cp[i][4], cp[i][5]).attr({
       fill: (i % 2 === 0) ? colors[0] : colors[1],
-      id: css_id + "i"
-      // filter: shadow
-    }).transform(rotationDegrees[i]);
+      id: css_id + "i",
+      filter: es ? shadow : ""
+    }).transform((stacked ? "" : rotationDegrees[i]));
   }
 }
 
@@ -102,7 +98,6 @@ _deckoLogo.animateSequence = function() {
   // setTimeout(function() { rotate("r0"); }, 2200);
   // setTimeout(function() { rotate(); }, 3400);
   var $this = this;
-  setTimeout(function() {
-    $this.changeToOriginal();
-  }, 1000);
+  setTimeout(function() { $this.rotate(); }, 1000);
+  setTimeout(function() { $this.changeToOriginal(); }, 2500);
 }
